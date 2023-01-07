5f9761ea5c5e075bd6d2b2225506d02ca109610e
Dennys Alvarenga <dennysnascimento01@gmail.com>
2023-01-07 20:50:34 -0300

    Lista.vue and Formulario.vue refatored

--- src/views/projects/Formulario.vue
+++ src/views/projects/Formulario.vue
@@ -13,9 +13,9 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import { useStore } from '@/store';
-import { ALTERA_PROJETO, ADICIONA_PROJETO } from '@/store/tipo-mutacoes';
 import { TipoNotificacao } from '@/interfaces/INotificacao';
 import useNotificador from '@/hooks/notificador';
+import { ALTERAR_PROJETO, CADASTRAR_PROJETO } from '@/store/tipo-acoes';
 
 export default defineComponent({
     name: 'Formulario',
@@ -24,33 +24,36 @@ export default defineComponent({
             type: String
         }
     },
-    mounted () {
-        if(this.id) {
+    mounted() {
+        if (this.id) {
             const projeto = this.store.state.projetos.find(proj => proj.id == this.id);
             this.nomeDoProjeto = projeto?.nome || '';
         }
     },
-    data () {
+    data() {
         return {
             nomeDoProjeto: ''
         };
     },
     methods: {
-        salvar () {
+        salvar() {
             if (this.id) {
-                this.store.commit(ALTERA_PROJETO, {
+                this.store.dispatch(ALTERAR_PROJETO, {
                     id: this.id,
                     nome: this.nomeDoProjeto
-                })
+                }).then(() => this.lidarComSucesso())
             } else {
-                this.store.commit(ADICIONA_PROJETO, this.nomeDoProjeto)
+                this.store.dispatch(CADASTRAR_PROJETO, this.nomeDoProjeto)
+                    .then(() => this.lidarComSucesso())
             }
+        },
+        lidarComSucesso() {
             this.nomeDoProjeto = '';
             this.notificar('Excelente!', 'O projeto foi cadastrado com sucesso', TipoNotificacao.SUCESSO)
             this.$router.push('/projetos');
         }
     },
-    setup () {
+    setup() {
         const store = useStore();
         const { notificar } = useNotificador();
         return {
