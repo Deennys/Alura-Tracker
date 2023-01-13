720aec7c85302027c7dd460a9cdf9f78e2b460a2
Dennys Alvarenga <dennysnascimento01@gmail.com>
2023-01-13 14:15:30 -0300

    Formulario.vue refatored

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -22,7 +22,7 @@
 </template>
 
 <script lang="ts">
-import { computed, defineComponent } from 'vue';
+import { computed, defineComponent, ref } from 'vue';
 import Temporizador from './Temporizador.vue';
 import { useStore } from 'vuex';
 
@@ -36,37 +36,41 @@ export default defineComponent({
     components: {
         Temporizador
     },
-    data () {
-        return {
-            descricao: '',
-            idProjeto: ''
-        }
-    },
     methods: {
-        finalizarTarefa (tempoDecorrido: number): void {
-            const projeto = this.projetos.find(p => p.id == this.idProjeto);
+        
+    },
+    setup (props, { emit }) {
+        const store = useStore(key);
+
+        const descricao = ref('');
+        const idProjeto = ref('');
+        const projetos = computed(() => store.state.projeto.projetos);
+
+        const finalizarTarefa = (tempoDecorrido: number): void => {
+            const projeto = projetos.value.find(p => p.id == idProjeto.value);
             console.log(projeto);
             if(!projeto) {
-                this.store.commit(NOTIFICAR, {
+                store.commit(NOTIFICAR, {
                 titulo: 'Ops!',
                 texto: 'Selecione um projeto antes de finalizar a tarefa!',
                 tipo: TipoNotificacao.FALHA
                 });
                 return;
             }
-            this.$emit('aoSalvarTarefa', {
+            emit('aoSalvarTarefa', {
                 duracaoEmSegundos: tempoDecorrido,
-                descricao: this.descricao,
-                projeto: this.projetos.find(proj => proj.id == this.idProjeto)
+                descricao: descricao.value,
+                projeto: projetos.value.find(proj => proj.id == idProjeto.value)
             })
-            this.descricao = '';
+            descricao.value = '';
         }
-    },
-    setup () {
-        const store = useStore(key);
+
         return {
-            projetos: computed(() => store.state.projeto.projetos),
-            store
+            descricao,
+            idProjeto,
+            projetos,
+            store,
+            finalizarTarefa
         }
     }
 });
