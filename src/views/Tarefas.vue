895dc3380e67cb1b7f8ef88c1b789741f5a25892
Dennys Alvarenga <dennysnascimento01@gmail.com>
2023-01-13 16:02:04 -0300

    Create search bar in Tarefas.vue

--- src/views/Tarefas.vue
+++ src/views/Tarefas.vue
@@ -4,6 +4,14 @@
     <Box v-if="listaEstaVazia">
       Você não está muito produtivo hoje :(
     </Box>
+    <div class="field">
+      <p class="control has-icons-left has-icons-right">
+        <input class="input" type="text" placeholder="Digite para filtrar" v-model="filtro">
+        <span class="icon is-small is-left">
+          <i class="fas fa-search"></i>
+        </span>
+      </p>
+    </div>
     <Tarefa v-for="(tarefa, index) in tarefas" :key="index" :tarefa="tarefa" @aoTarefaClicada="selecionarTarefa" />
     <div class="modal" :class="{ 'is-active': tarefaSelecionada }" v-if="tarefaSelecionada">
       <div class="modal-background"></div>
@@ -19,7 +27,7 @@
           </div>
         </section>
         <footer class="modal-card-foot">
-          <button class="button is-success" @click="alterarTarefa" >Salvar alterações</button>
+          <button class="button is-success" @click="alterarTarefa">Salvar alterações</button>
           <button class="button" @click="fecharModal">Cancelar</button>
         </footer>
       </div>
@@ -28,7 +36,7 @@
 </template>
 
 <script lang="ts">
-import { computed, defineComponent } from 'vue';
+import { computed, defineComponent, ref } from 'vue';
 import Formulario from '../components/Formulario.vue';
 import Tarefa from '../components/Tarefa.vue';
 import Box from '../components/Box.vue';
@@ -76,9 +84,16 @@ export default defineComponent({
     const store = useStore();
     store.dispatch(OBTER_TAREFAS);
     store.dispatch(OBTER_PROJETOS);
+
+    const filtro = ref('');
+
+    const tarefas = computed(() => store.state.tarefa.tarefas
+      .filter(t => !filtro.value || t.descricao.toLowerCase().includes(filtro.value.toLowerCase()) || t.projeto.nome.toLowerCase().includes(filtro.value.toLowerCase())))
+
     return {
-      tarefas: computed(() => store.state.tarefa.tarefas),
-      store
+      tarefas,
+      store,
+      filtro
     }
   }
 });
