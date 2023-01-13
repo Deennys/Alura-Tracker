32530e728f945571cd714cde63378c810f0e13c3
Dennys Alvarenga <dennysnascimento01@gmail.com>
2023-01-13 16:13:14 -0300

    Refatored search bar logic

--- src/views/Tarefas.vue
+++ src/views/Tarefas.vue
@@ -36,7 +36,7 @@
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, ref } from 'vue';
+import { computed, defineComponent, ref, watchEffect } from 'vue';
 import Formulario from '../components/Formulario.vue';
 import Tarefa from '../components/Tarefa.vue';
 import Box from '../components/Box.vue';
@@ -87,11 +87,15 @@ export default defineComponent({
 
     const filtro = ref('');
 
-    const tarefas = computed(() => store.state.tarefa.tarefas
-      .filter(t => !filtro.value || t.descricao.toLowerCase().includes(filtro.value.toLowerCase()) || t.projeto.nome.toLowerCase().includes(filtro.value.toLowerCase())))
+//    const tarefas = computed(() => store.state.tarefa.tarefas
+//      .filter(t => !filtro.value || t.descricao.toLowerCase().includes(filtro.value.toLowerCase()) || t.projeto.nome.toLowerCase().includes(filtro.value.toLowerCase())));
+
+    watchEffect(() => {
+      store.dispatch(OBTER_TAREFAS, filtro.value);
+    })
 
     return {
-      tarefas,
+      tarefas: computed(() => store.state.tarefa.tarefas),
       store,
       filtro
     }
